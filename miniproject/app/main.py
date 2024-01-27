53570310b4e554dca82d74be0de2d6ac210798e0
thejk726 <thejkiran726@gmail.com>
2024-01-27 23:18:51 +0530

    View projects and View assigned students functionality added

--- miniproject/app/main.py
+++ miniproject/app/main.py
@@ -1,3 +1,6 @@
+#-------------------------------------------------* MENTOR FUNCTIONALITIES *----------------------------------------------------------
+
+
 import psycopg2
 from psycopg2 import sql
 from flask import Flask,request,jsonify
@@ -6,6 +9,7 @@ from miniproject.config import db_params
 
 app=Flask(__name__)
 
+#-----------------------------------------------------FUNCTIONS------------------------------------------------------------------------
 
 # Function to update mentor profile
 def update_mentor_profile(m_id,qualification):
@@ -17,8 +21,32 @@ def update_mentor_profile(m_id,qualification):
         return True
     except psycopg2.error as e:
         return False
+    
 
+# Function to fetch projects under a mentor
+def get_mentor_projects(mentorid):
+    try:
+        with psycopg2.connect(**db_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''SELECT * FROM project WHERE m_id=%s''',(mentorid))
+                projects=cursor.fetchall()
+        return projects
+    except psycopg2.Error as e:
+        return None
 
+#Function to fetch students under each projects
+def get_project_students(project_id):
+    try:
+        with psycopg2.connect(**db_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('SELECT * FROM student WHERE st_id IN(SELECT st_id FROM st_project WHERE prj_id=%s)',(project_id))
+                students_in_project=cursor.fetchall()
+        return students_in_project
+    except psycopg2.Error as e:
+        return None
+
+#------------------------------------------------------ROUTES-------------------------------------------------------------------------------
+    
 # Route to update mentor profile
 @app.route('/mentor/<string:username>/profile',method=['PUT'])
 def update_profile(m_id):
@@ -32,4 +60,22 @@ def update_profile(m_id):
         return jsonify({'message':'Profile updated successfully!'})
     else:
         return jsonify({'error':'failed to update mentor profile!'}),500
+    
+#Route to get projects under the logged in mentor
+@app.route('/mentor/<string:mentor_id>/projects',method=['GET'])
+def mentor_projects():
+    mentor_id=request.args.get('mentor_id') # Fetches the corresponding value for mentor_id from the URL
+    projects=get_mentor_projects(mentor_id)
+    return jsonify(projects)
 
+#Route to get students under a given project
+@app.route('/mentor/<string:m_id/projects/<string:project_id>',methods=['GET'])
+def project_students():
+    projectid=request.args.get('project_id')
+    students=get_project_students(projectid)
+    return jsonify(students)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
